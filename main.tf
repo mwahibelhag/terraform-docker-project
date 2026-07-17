3b5182d6e6bc5b36ea4a85cfd0e2795fc5fef1c4
Mwahib Mohamed Elhag <mwahibmohelhag@gmail.com>
2026-07-17 05:04:51 +0300

    Add main.tf configuration

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_server" {
+  name  = "nginx-server"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
